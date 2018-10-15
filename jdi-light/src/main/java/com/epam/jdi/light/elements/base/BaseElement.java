e980928b6374b16a9576752fba8f44f73f5b71b3
Aleksandr Chilikin <aleksandr_chilikin@epam.com>
2018-10-15 14:22:23 +0300

    small fix

--- jdi-light/src/main/java/com/epam/jdi/light/elements/base/BaseElement.java
+++ jdi-light/src/main/java/com/epam/jdi/light/elements/base/BaseElement.java
@@ -1,12 +1,19 @@
 package com.epam.jdi.light.elements.base;
 
+import com.epam.jdi.light.common.JDIAction;
 import com.epam.jdi.light.elements.interfaces.HasValue;
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.Select;
 
 public interface BaseElement extends HasValue {
+
+    @JDIAction
     void hover();
+
+    @JDIAction
     boolean isEnabled();
+
+    @JDIAction
     boolean isDisplayed();
     Point getLocation();
     Dimension getSize();
@@ -15,8 +22,14 @@ public interface BaseElement extends HasValue {
     String getAttribute(String name);
     void setAttribute(String name, String value);
     void higlight(String color);
+
+    @JDIAction
     void higlight();
+
+    @JDIAction
     void show();
+
+    @JDIAction
     Select select();
 
     default UIElement base() {return (UIElement) this; }
